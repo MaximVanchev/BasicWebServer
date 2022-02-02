a5f080e69e4c43122332c84a657371e0d32164bf
BuildTools <unconfigured@null.spigotmc.org>
2022-02-02 21:38:11 +0200

    Adding view response

---
+++ BasicWebServer.Server/Responses/ContentResponses/ViewResponse.cs
@@ -0,0 +1,56 @@
+﻿using BasicWebServer.Server.HTTP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server.Responses.ContentResponses
+{
+    public class ViewResponse : ContentResponse
+    {
+        private const char PathSeparator = '/';
+
+        public ViewResponse(string viewName, string controllerName , object model = null) 
+            : base("" , ContentType.Html)
+        {
+            if (!viewName.Contains(PathSeparator))
+            {
+                viewName = controllerName + PathSeparator + viewName;
+            }
+
+            var viewPath = Path.GetFullPath($"./Views/{viewName.TrimStart(PathSeparator)}.cshtml");
+
+            var viewContent = File.ReadAllText(viewPath);
+
+            if (model != null)
+            {
+                viewContent = PopuleteModel(viewContent, model);
+            }
+
+            Body = viewContent;
+        }
+
+        private string PopuleteModel(string viewContent, object model)
+        {
+            var data = model
+                .GetType()
+                .GetProperties()
+                .Select(p => new
+                {
+                    p.Name,
+                    Value = p.GetValue(model)
+                });
+
+            foreach (var item in data)
+            {
+                const string openingBrackets = "{{";
+                const string closingBrackets = "}}";
+
+                viewContent = viewContent.Replace($"{openingBrackets}{item.Name}{closingBrackets}" , item.Value.ToString());
+            }
+
+            return viewContent;
+        }
+    }
+}
